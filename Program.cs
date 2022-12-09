3d93a3b24ea80aca8caa5bdcef018e91176ea000
forcesumetee <46419997+forcesumetee@users.noreply.github.com>
2022-12-09 17:06:28 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void BuyBook(Stack<string> smartShelf, ref string transaction)
+    {
+        string book = Console.ReadLine();
+        Queue<string> tmpShelf = new Queue<string>();
+        while(smartShelf.GetLength() > 0)
+        {
+            string currentBook = smartShelf.Pop();
+            transaction = "Valid";
+            if(currentBook == book)
+            {
+                
+                break;
+                
+            }
+            else
+            {
+                tmpShelf.Push(currentBook);
+            }
+        }
+
+        while(tmpShelf.GetLength() > 0)
+        {
+            smartShelf.Push(tmpShelf.Pop());
+            transaction = "Invalid";
+        }
+    }
+
+    static void InputBook(Stack<string> smartShelf)
+    {
+        string book;
+        while(true)
+        {
+            book = Console.ReadLine();
+            if(book == "Stop")
+            {
+                break;
+            }
+            smartShelf.Push(book);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Stack<string> smartShelf = new Stack<string>();
+        InputBook(smartShelf);
+
+        int transaction = 0;
+        while(smartShelf.GetLength() > 0)
+        {
+            BuyBook(smartShelf,  ref transaction);
+        }
+
+        Console.WriteLine(transaction);
+    }
+}
